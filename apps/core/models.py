c4eade49f732f0abbfdf6a165f671e3d23ba7dba
Mohamad-sudo616 <mohamadamini4884@gmail.com>
2025-12-19 15:14:30 +0330

    change

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# You can add models later
+
+class Profile(models.Model):
+    full_name = models.CharField(max_length=100)
+    title = models.CharField(max_length=150)
+    bio = models.TextField()
+    profile_image = models.ImageField(
+        upload_to="profiles/",
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return self.full_name
